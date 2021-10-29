95297643f179e949c223cb2fc38c3fed0d198046
Ilya Sereda <ilya.sereda2002@mail.ru>
2021-10-29 18:03:33 +0300

    testing smth

--- main/views.py
+++ main/views.py
@@ -1,28 +1,28 @@
 from django.db import connection
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from main.models import Ads
 
 
 def index(request):
-    # cursor = connection.cursor()
-    # cursor.execute("SELECT * FROM ads")
-    # row = cursor.fetchall()
-    # return HttpResponse(row)
     ads = Ads.objects.all()
     context = {
         'ads': ads
     }
-    return render(request, 'example.html', context)
+    return render(request, 'main/ads.html', context)
 
 
 def ad(request, ad_id):
-    ads = Ads.objects.filter(id_ad=ad_id)
-    context = {
+    if ad_id > len(Ads.objects.all()) or ad_id <= 0:
+        raise Http404("Такого объявления не существует... Попробуйте что-нибудь другое")
+    else:
+        ads = Ads.objects.filter(id_ad=ad_id)
+        context = {
         'ads': ads
     }
-    return render(request, 'example.html', context)
+    return render(request, 'main/ads.html', context)
+    
 
 def about(request):
     return HttpResponse('about')
