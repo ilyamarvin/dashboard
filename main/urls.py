e1e780ed3bfebdb826e5e43af17abe8e616b3f03
Ilya Sereda <ilya.sereda2002@mail.ru>
2021-10-30 13:39:05 +0300

    added searching query

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main'),
+    path('ads/', views.ads, name='ads'),
     path('ad/<int:ad_id>/', views.ad),
-    path('about/', views.about),
+    path('add/', views.add, name='add_new'),
+    path('about/', views.about, name='about'),
+    path('personal/', views.personal, name='personal'),
+    path('search/', views.SearchResultsList.as_view(), name='search_results')
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
 from django.db import connection
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.db.models import Q
+from django.views.generic import ListView
 
 from main.models import Ads
 
 
 def index(request):
+    num_ads = Ads.objects.all().count()
+    context = {
+        'num_ads': num_ads
+    }
+    return render(request, 'index.html', context)
+
+
+def ads(request):
     ads = Ads.objects.all()
     context = {
         'ads': ads
@@ -22,7 +32,26 @@ def ad(request, ad_id):
         'ads': ads
     }
     return render(request, 'main/ads.html', context)
+
+
+def add(request):
+    return HttpResponse('add new ad')
+
+
+def personal(request):
+    return HttpResponse('personal information')
     
 
 def about(request):
     return HttpResponse('about')
+
+class SearchResultsList(ListView):
+    model = Ads
+    context_object_name = "ads"
+    template_name = "main/ads.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Ads.objects.filter(
+            Q(name__icontains=query)
+        )
